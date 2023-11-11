2e3fbe486e2818d341827b5aa2622d8f1ea214d0
leon <44578198+Cyb-Leon@users.noreply.github.com>
2023-11-11 21:06:23 +0200

    API connections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Crowdfunder from "./pages/crowdfunder";
+import Accounts from "./pages/accounts";
+import Rewards from "./pages/rewards";
+import Transaction from "./pages/transactions";
+import Signin from "./pages/signin";
+import Nopage from "./pages/nopage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="rewards" element={<Rewards />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="*" element={<Nopage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
