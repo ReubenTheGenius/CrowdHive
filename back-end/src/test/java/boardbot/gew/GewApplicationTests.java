f4f9950f65f83f5c414ea73e3d3148f6540d016a
leon <44578198+Cyb-Leon@users.noreply.github.com>
2023-11-11 09:24:22 +0200

    init projx

---
+++ back-end/src/test/java/boardbot/gew/GewApplicationTests.java
@@ -0,0 +1,13 @@
+package boardbot.gew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
